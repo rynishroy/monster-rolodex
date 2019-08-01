7543038c77b121cba7b1cd6d2bbc8bd607903bf9
roysalda <rynish.roy_saldanha@nokia.com>
2019-08-01 09:41:21 +0530

    Updates

---
+++ precache-manifest.fecd49c22ed27bd8e0f602d18f684125.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b39c652d793814b805209490a0f72e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4725afffab7468a7ec16",
+    "url": "/monster-rolodex/static/css/main.f415af50.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4725afffab7468a7ec16",
+    "url": "/monster-rolodex/static/js/main.1c993565.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
